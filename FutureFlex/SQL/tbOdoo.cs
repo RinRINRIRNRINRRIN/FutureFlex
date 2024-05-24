06ccd5272d421212193f1fb570694d6ba4accd31
RinRINRIRNRINRRIN <karinking9078loop@gmail.com>
2024-05-24 12:59:37 +0700

    แก้ไขบรรทัด

--- FutureFlex/SQL/tbOdoo.cs
+++ FutureFlex/SQL/tbOdoo.cs
@@ -8,7 +8,6 @@ namespace FutureFlex.SQL
 
         public static string key { get; set; }
         public static string server { get; set; }
-
         public static string db { get; set; }
 
         public static void defineServerOdoo()
@@ -28,8 +27,6 @@ namespace FutureFlex.SQL
                     server = rw["od_server"].ToString();
                     db = rw["od_database"].ToString();
                 }
-
-
             }
             catch (System.Exception ex)
             {
