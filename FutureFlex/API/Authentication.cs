dbef2ca4e1d04d69ccff0d14cde6c33686cf767b
RinRINRIRNRINRRIN <karinking9078loop@gmail.com>
2024-07-25 09:14:59 +0700

    เพิ่ม log

--- FutureFlex/API/Authentication.cs
+++ FutureFlex/API/Authentication.cs
@@ -1,6 +1,7 @@
 ﻿using FutureFlex.SQL;
 using Newtonsoft.Json.Linq;
 using RestSharp;
+using Serilog;
 using System;
 using System.Threading.Tasks;
 namespace FutureFlex.API
@@ -11,9 +12,9 @@ namespace FutureFlex.API
         public static string ERR { get; set; }
         public async static Task<bool> take_token_key()
         {
-
             try
             {
+                Log.Information("== เช็ค token");
                 var options = new RestClientOptions(tbOdoo.server)
                 {
                     MaxTimeout = -1,
@@ -24,7 +25,7 @@ namespace FutureFlex.API
                 request.AddHeader("key", tbOdoo.key);
                 RestResponse response = await client.ExecuteAsync(request);
                 Console.WriteLine(response.Content);
-
+                Log.Information($"- response \n {response.Content}");
                 if (response.StatusCode != System.Net.HttpStatusCode.OK)
                 {
                     return false;
@@ -37,8 +38,10 @@ namespace FutureFlex.API
             catch (Exception ex)
             {
                 ERR = ex.Message;
+                Log.Information($"take_token_key | Authenticaion : {ERR}");
                 return false;
             }
+            Log.Information($"- เช็ค Token สำเร็จ");
             return true;
         }
 
