0e126baeddaf53591f6c4dd236e86761452cf9fb
RinRINRIRNRINRRIN <karinking9078loop@gmail.com>
2025-01-24 15:27:06 +0700

    เพิ่มหน้า Tcpclient

---
+++ FutureFlex/frmTcpClientServer.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace FutureFlex
+{
+    partial class frmTcpClientServer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmTcpClientServer));
+            this.guna2BorderlessForm1 = new Guna.UI2.WinForms.Guna2BorderlessForm(this.components);
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.txtMessage = new Guna.UI2.WinForms.Guna2TextBox();
+            this.btnConnect = new Guna.UI2.WinForms.Guna2Button();
+            this.guna2Button1 = new Guna.UI2.WinForms.Guna2Button();
+            this.SuspendLayout();
+            // 
+            // guna2BorderlessForm1
+            // 
+            this.guna2BorderlessForm1.ContainerControl = this;
+            this.guna2BorderlessForm1.DockIndicatorColor = System.Drawing.Color.Teal;
+            this.guna2BorderlessForm1.DockIndicatorTransparencyValue = 0.6D;
+            this.guna2BorderlessForm1.ShadowColor = System.Drawing.Color.Teal;
+            this.guna2BorderlessForm1.TransparentWhileDrag = true;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(8, 51);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(515, 430);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txtMessage.DefaultText = "";
+            this.txtMessage.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.txtMessage.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.txtMessage.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txtMessage.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txtMessage.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txtMessage.Font = new System.Drawing.Font("Athiti", 12F);
+            this.txtMessage.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txtMessage.IconRight = ((System.Drawing.Image)(resources.GetObject("txtMessage.IconRight")));
+            this.txtMessage.Location = new System.Drawing.Point(8, 489);
+            this.txtMessage.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
+            this.txtMessage.Multiline = true;
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.PasswordChar = '\0';
+            this.txtMessage.PlaceholderText = "IP SERVER";
+            this.txtMessage.SelectedText = "";
+            this.txtMessage.Size = new System.Drawing.Size(515, 117);
+            this.txtMessage.TabIndex = 4;
+            this.txtMessage.TextChanged += new System.EventHandler(this.guna2TextBox3_TextChanged);
+            this.txtMessage.KeyDown += new System.Windows.Forms.KeyEventHandler(this.guna2TextBox3_KeyDown);
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btnConnect.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btnConnect.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btnConnect.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btnConnect.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.btnConnect.ForeColor = System.Drawing.Color.White;
+            this.btnConnect.Location = new System.Drawing.Point(8, 9);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(456, 36);
+            this.btnConnect.TabIndex = 2;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.Click += new System.EventHandler(this.guna2Button1_Click);
+            // 
+            // guna2Button1
+            // 
+            this.guna2Button1.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.guna2Button1.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.guna2Button1.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.guna2Button1.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.guna2Button1.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.guna2Button1.ForeColor = System.Drawing.Color.White;
+            this.guna2Button1.Location = new System.Drawing.Point(473, 9);
+            this.guna2Button1.Name = "guna2Button1";
+            this.guna2Button1.Size = new System.Drawing.Size(50, 36);
+            this.guna2Button1.TabIndex = 5;
+            this.guna2Button1.Text = "X";
+            this.guna2Button1.Click += new System.EventHandler(this.guna2Button1_Click_1);
+            // 
+            // frmTcpClientServer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(535, 617);
+            this.Controls.Add(this.guna2Button1);
+            this.Controls.Add(this.txtMessage);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.btnConnect);
+            this.Font = new System.Drawing.Font("Athiti", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(5, 7, 5, 7);
+            this.Name = "frmTcpClientServer";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "frmTcpClientServer";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2BorderlessForm guna2BorderlessForm1;
+        private Guna.UI2.WinForms.Guna2TextBox txtMessage;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private Guna.UI2.WinForms.Guna2Button guna2Button1;
+        private Guna.UI2.WinForms.Guna2Button btnConnect;
+    }
+}
---
+++ FutureFlex/frmTcpClientServer.cs
@@ -0,0 +1,114 @@
+﻿using FutureFlex.Function;
+using System;
+using System.Drawing;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FutureFlex
+{
+    public partial class frmTcpClientServer : Form
+    {
+        public frmTcpClientServer()
+        {
+            InitializeComponent();
+        }
+
+        private async void guna2Button1_Click(object sender, EventArgs e)
+        {
+            switch (btnConnect.Text)
+            {
+                case "Connect":
+                    btnConnect.Enabled = false;
+                    if (await func_tcpClient.Connect())
+                    {
+
+                        btnConnect.Text = "Disconnect";
+                        _ = Task.Run(() =>
+                        {
+                            GetMessage();
+                        });
+                    }
+                    else
+                    {
+                        MessageBox.Show(func_tcpClient.ERR);
+                    }
+                    btnConnect.Enabled = true;
+                    break;
+                case "Disconnect":
+                    if (await func_tcpClient.Disconnect())
+                    {
+                        btnConnect.Text = "Connect";
+                    }
+                    else
+                    {
+                        MessageBox.Show(func_tcpClient.ERR);
+                    }
+                    break;
+            }
+        }
+
+        private void guna2TextBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        async void GetMessage()
+        {
+            string old_data = "";
+            while (true)
+            {
+                if (func_tcpClient.ConnectState)
+                {
+                    string message = func_tcpClient.ReceiveData;
+                    if (message != null)
+                    {
+                        BeginInvoke(new MethodInvoker(delegate ()
+                        {
+                            richTextBox1.SelectionColor = Color.Blue;
+                            richTextBox1.AppendText(message);
+                            richTextBox1.ScrollToCaret();
+                        }));
+                        func_tcpClient.ReceiveData = null;
+                    }
+                }
+                else
+                {
+                    if (btnConnect.Text == "Disconnect")
+                    {
+                        MessageBox.Show("Server is offline");
+                        BeginInvoke(new MethodInvoker(delegate ()
+                        {
+                            btnConnect.Text = "Connect";
+                        }));
+                    }
+                    return;
+                }
+
+
+                await Task.Delay(500);
+            }
+        }
+
+        private async void guna2TextBox3_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                if (txtMessage.Text != "")
+                {
+                    if (await func_tcpClient.SendDataAsync(txtMessage.Text))
+                    {
+                        richTextBox1.SelectionColor = Color.Red;
+                        richTextBox1.AppendText($"Me : {txtMessage.Text}");
+                        richTextBox1.ScrollToCaret();
+                        txtMessage.Clear();
+                    }
+                }
+            }
+        }
+
+        private void guna2Button1_Click_1(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
