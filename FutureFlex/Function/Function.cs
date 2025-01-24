6a30116dfc16140b3390847255df00eab33b2d8e
RinRINRIRNRINRRIN <karinking9078loop@gmail.com>
2025-01-24 15:24:45 +0700

    ลบฟังชั่น RS232 ออก

--- FutureFlex/Function/Function.cs
+++ FutureFlex/Function/Function.cs
@@ -1,6 +1,4 @@
-﻿using System.Configuration;
-using System.Diagnostics;
-using System.IO.Ports;
+﻿using System.Diagnostics;
 
 namespace FutureFlex.Function
 {
@@ -29,38 +27,5 @@ namespace FutureFlex.Function
             }
             return true;
         }
-
-
-        public static string RS232(SerialPort sa)
-        {
-            string d = "";
-            string device = ConfigurationManager.AppSettings["DEVICE"];
-
-            switch (device)
-            {
-                case "DI81":
-                    string data = sa.ReadLine();        //// นำค่าที่ได้จาก RS232 มาเก็บไว้ในตัวแปร
-
-                    string[] value = data.Split('\r');
-                    //Console.WriteLine("GROSS WEIGHT " + value[4].Substring(12).Trim());
-                    //Console.WriteLine("NET WEIGHT " + value[5].Substring(10).Trim());
-                    //Console.WriteLine("TARE " + value[6].Substring(4).Trim());
-
-                    string net = value[5].Substring(10).Trim();
-                    string tare = value[6].Substring(4).Trim();
-                    string gross = value[4].Substring(12).Trim();
-
-                    d = $"{net}|{tare}|{gross}";
-                    break;
-                case "DFWLB-SN405":
-
-                    break;
-                case "DFWL-SN405":
-
-                    break;
-            }
-            return d;
-        }
-
     }
 }
