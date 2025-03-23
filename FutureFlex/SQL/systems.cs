0f40cd1a583731dd9b3cfea5585296cc7125d6a2
RinRINRIRNRINRRIN <karinking9078loop@gmail.com>
2025-03-23 13:21:00 +0700

    Add systems class

---
+++ FutureFlex/SQL/systems.cs
@@ -0,0 +1,46 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+namespace FutureFlex.SQL
+{
+    internal class systems
+    {
+        static SqlConnection con = server.con;
+        static SqlDataAdapter da = new SqlDataAdapter();
+        static DataTable tb = new DataTable();
+        static SqlCommand cmd = new SqlCommand();
+        static string sql = "";
+
+        public static string Versoin
+        {
+            get { return "1.3.0"; }
+        }
+
+        public static bool CheckVersion()
+        {
+            try
+            {
+                sql = "SELECT * FROM tbSystem";
+                da = new SqlDataAdapter(sql, con);
+                tb = new DataTable();
+                da.Fill(tb);
+
+                foreach (DataRow rw in tb.Rows)
+                {
+                    string ver = rw["version"].ToString();
+                    // เช็คว่าตรงกันหรือไม่
+                    if (Versoin != ver)
+                    {
+                        return false;
+                    }
+                }
+            }
+            catch
+            {
+                return false;
+            }
+            return true;
+        }
+
+    }
+}
