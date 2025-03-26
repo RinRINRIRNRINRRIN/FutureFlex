0ec8a7ed9ed72948b9c5f54d4d57fdd7cdb0486a
RinRINRIRNRINRRIN <karinking9078loop@gmail.com>
2025-03-26 20:18:51 +0700

    Add EmployeeModel

---
+++ FutureFlex/Models/EmployeeModel.cs
@@ -0,0 +1,25 @@
+﻿namespace FutureFlex.Models
+{
+    internal class EmployeeModel
+    {
+        /// <summary>
+        /// username
+        /// </summary>
+        public static string emp_username { get; set; }
+
+        /// <summary>
+        /// Full name
+        /// </summary>
+        public static string emp_name { get; set; }
+
+        /// <summary>
+        /// Password
+        /// </summary>
+        public static string emp_password { get; set; }
+
+        /// <summary>
+        /// ERR
+        /// </summary>
+        public static string ERR { get; set; }
+    }
+}
