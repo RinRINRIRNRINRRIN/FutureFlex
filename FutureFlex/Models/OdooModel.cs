5dcbe6b95eeb523f2281282363e9b4d4d7cb23e2
RinRINRIRNRINRRIN <karinking9078loop@gmail.com>
2025-04-29 11:13:46 +0700

    Add OdooModel

---
+++ FutureFlex/Models/OdooModel.cs
@@ -0,0 +1,19 @@
+﻿namespace FutureFlex.Models
+{
+    public class OdooModel
+    {
+        /// <summary>
+        /// Product or UAT
+        /// </summary>
+        public static string Status { get; set; }
+
+
+        public static string Server { get; set; }
+
+
+        public static string Key { get; set; }
+
+        public static string Database { get; set; }
+
+    }
+}
