67f6059faf8fed084218f66ab82710d02d3f6a3a
RinRINRIRNRINRRIN <karinking9078loop@gmail.com>
2025-04-29 11:12:26 +0700

    SPLIT แก้ไข Header ให้ดึงข้อมูลจาก OdooModel

--- FutureFlex/API/SLP.cs
+++ FutureFlex/API/SLP.cs
@@ -1,4 +1,4 @@
-﻿using FutureFlex.SQL;
+﻿using FutureFlex.Models;
 using Newtonsoft.Json.Linq;
 using RestSharp;
 using Serilog;
@@ -127,14 +127,14 @@ namespace FutureFlex.API
                 JObject keys = new JObject();
                 JArray jArray = new JArray();
 
-                var options = new RestClientOptions(tbOdoo.server)
+                var options = new RestClientOptions(OdooModel.Server)
                 {
                     MaxTimeout = 3000,
                 };
                 var client = new RestClient(options);
                 var request = new RestRequest("/api/split_list", Method.Get);
-                request.AddHeader("key", tbOdoo.key);
-                request.AddHeader("po_num", po_num);
+                request.AddHeader("key", OdooModel.Key);
+                request.AddHeader("po-num", po_num);
                 RestResponse response = await client.ExecuteAsync(request);
                 Console.WriteLine(response.Content);
 
@@ -182,14 +182,14 @@ namespace FutureFlex.API
             try
             {
                 CreateDataTable();
-                var options = new RestClientOptions(tbOdoo.server)
+                var options = new RestClientOptions(OdooModel.Server)
                 {
                     MaxTimeout = 3000,
                 };
                 var client = new RestClient(options);
                 var request = new RestRequest("/api/split_num", Method.Get);
-                request.AddHeader("key", tbOdoo.key);
-                request.AddHeader("split_num", split_num);
+                request.AddHeader("key", OdooModel.Key);
+                request.AddHeader("split-num", split_num);
                 RestResponse response = await client.ExecuteAsync(request);
                 Console.WriteLine(response.Content);
 
@@ -280,13 +280,13 @@ namespace FutureFlex.API
                     po = "JIT";
                 }
 
-                var options = new RestClientOptions(tbOdoo.server)
+                var options = new RestClientOptions(OdooModel.Server)
                 {
                     MaxTimeout = -1,
                 };
                 var client = new RestClient(options);
                 var request = new RestRequest("/api/split_num", Method.Post);
-                request.AddHeader("key", tbOdoo.key);
+                request.AddHeader("key", OdooModel.Key);
                 request.AddHeader("split_num", spl_name);
                 request.AddHeader("Content-Type", "text/plain");
                 var body = "{\n" +
